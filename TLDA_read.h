e3c8115309f71d3c689d4c2609156b84c7df5aa8
Masoniced <sen_mei@mymail.sutd.edu.sg>
2020-01-01 20:28:09 +0800

    Update and rename TLDA.h to TLDA_read.h

---
+++ TLDA_read.h
@@ -0,0 +1,21 @@
+#ifndef _TLDA_READ_H
+#define _TLDA_READ_H
+
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <map>
+
+typedef std::unordered_map < std::string, std::vector<int> > SPARSE_LABEL;
+typedef std::unordered_map <std::string, SPARSE_LABEL > SPARSE_DATA;
+typedef std::unordered_map <std::string, int> COLUMN_INDEX;
+typedef std::map<int, std::string> INDEX_COLUMN;
+
+extern INDEX_COLUMN index_words;
+extern int total_transaction_count;
+extern SPARSE_DATA train_sparse_data;
+extern COLUMN_INDEX column_index;
+
+extern int read_process(std::string const path, std::string const name, int num_test);
+
+#endif
